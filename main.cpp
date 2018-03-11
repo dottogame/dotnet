bcb15293d2eb3c8dae8e077b937a2d2188c7a094
lite20 <lite260@gmail.com>
2018-03-11 01:47:01 -0600

    receive data

--- main.cpp
+++ main.cpp
@@ -5,14 +5,24 @@
 
 int main(int argc, char **argv)
 {
+    bool running = true;
     sf::UdpSocket socket;
     
     if (socket.bind(PORT) != sf::Socket::Done)
     {
         // TODO: Handle error
     }
-
-    socket.setBlocking(false);
     
+    char data[BUFF_SIZE];
+    std::size_t received;
+    sf::IpAddress sender;
+    unsigned short port;
+
+    while (running) {
+        // lol for now we just die if an error occurs
+        if (socket.receive(data, BUFF_SIZE, received, sender, port) != sf::Socket::Done) return 1;
+
+    }
+
     return 0;
 }
