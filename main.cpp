d97df9ea49281a9c7b890155cc1792ebaa6c1ff1
Shadow <sendmecrap@outlook.com>
2018-03-10 23:01:26 -0500

    Started working on the Server

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <SFML\Network.hpp>
 
+#define PORT 46980
+#define BUFF_SIZE 128
+
 int main(int argc, char **argv)
 {
+    sf::UdpSocket socket;
+    
+    if (socket.bind(PORT) != sf::Socket::Done)
+    {
+        // TODO: Handle error
+    }
+    
     return 0;
 }
