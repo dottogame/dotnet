9332b57ebbd5913fac24a54ae0bb01a9d3a3cfef
lite20 <lite260@gmail.com>
2018-03-11 02:26:57 -0600

    stage all fils

---
+++ AuthKit.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <SFML\Network.hpp>
+
+#include "TwoStateDotocol.hpp"
+
+namespace authkit {
+    void check(char* data, sf::IpAddress& sender, unsigned short& port)
+    {
+        // TODO check
+        // TODO build connection object if correct
+    }
+}
