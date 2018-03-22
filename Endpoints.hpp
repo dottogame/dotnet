a82ce5072b8209ceb97d7e4e93c9e250c09906f7
lite20 <lite260@gmail.com>
2018-03-22 13:04:37 -0600

    make lines short again

--- Endpoints.hpp
+++ Endpoints.hpp
@@ -7,12 +7,16 @@
 
 namespace endpoint
 {
-    void ping(sf::IpAddress& sender, unsigned short& port, sf::UdpSocket& socket)
+    void ping(
+        sf::IpAddress& sender, unsigned short& port, sf::UdpSocket& socket
+    )
     {
         socket.send("pong!", 5, sender, port);
     }
 
-    void lobby_count(sf::IpAddress& sender, unsigned short& port, config* configuration)
+    void lobby_count(
+        sf::IpAddress& sender, unsigned short& port, config* configuration
+    )
     {
         // the key to get an id is the ip and port concatenated
         std::string key = sender.toString() + std::to_string(port);
@@ -31,7 +35,10 @@ namespace endpoint
         else tsd::pack(con, "rl0");
     }
 
-    void lobby_details(char* data, sf::IpAddress& sender, unsigned short& port, config* configuration)
+    void lobby_details(
+        char* data, sf::IpAddress& sender, unsigned short& port,
+        config* configuration
+    )
     {
         // the key to get an id is the ip and port concatenated
         std::string key = sender.toString() + std::to_string(port);
@@ -50,7 +57,10 @@ namespace endpoint
         else tsd::pack(con, "rle:this server has no lobbies");
     }
 
-    void get_stack(char* data, sf::IpAddress& sender, unsigned short& port, sf::UdpSocket& socket)
+    void get_stack(
+        char* data, sf::IpAddress& sender, unsigned short& port,
+        sf::UdpSocket& socket
+    )
     {
         // the key to get an id is the ip and port concatenated
         std::string key = sender.toString() + std::to_string(port);
--- main.cpp
+++ main.cpp
@@ -12,22 +12,21 @@ bool running = true;
 /**
  * take request and route to correct endpoint handle
 */
-void route(char* data, sf::IpAddress& sender, unsigned short& port, sf::UdpSocket& socket, config* configuration)
-{ 
-    // pinged
+void route(
+    char* data,
+    sf::IpAddress& sender,
+    unsigned short& port,
+    sf::UdpSocket& socket,
+    config* configuration
+)
+{
     if (data[0] == '!') endpoint::ping(sender, port, socket);
-
-    // check if packet is a request
     else if (data[0] == 'r')
     {
-        // request lobby count
         if (data[1] == 'l' && data[2] == 'c') endpoint::lobby_count(sender, port, configuration);
-
-        // request lobby details
         if (data[1] == 'l' && data[2] == 'd') endpoint::lobby_details(data, sender, port, configuration);
     }
 
-    // client is requesting response stack
     else endpoint::get_stack(data, sender, port, socket);
 }
 
@@ -46,13 +45,16 @@ int main(int argc, char **argv)
     configuration = new config("./config.json");
 
     // bind to port. exit if failed.
-    if (socket.bind(configuration->port) != sf::Socket::Done) return 1;
+    if (socket.bind(configuration->port) != sf::Socket::Done)
+        return 1;
 
     // loop recieving packets
     while (running)
     {
         // recieve message and die if an error occurs
-        if (socket.receive(data, BUFF_SIZE, received, sender, port) != sf::Socket::Done) return 1;
+        if (socket.receive(
+            data, BUFF_SIZE, received, sender, port
+        ) != sf::Socket::Done) return 1;
 
         // if no prefix, check auth and make connection
         if (data[0] == '-') authkit::check(data, sender, port);
